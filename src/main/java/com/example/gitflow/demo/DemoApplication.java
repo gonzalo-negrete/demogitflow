49bdb9091390e76f705ee80adfd68d5d3ca85a1e
MONEX_VARSOVIA\gguerreroj_ext <gguerreroj_ext@monex.com.mx>
2024-10-11 18:42:57 -0600

    enviando cambios

--- src/main/java/com/example/gitflow/demo/DemoApplication.java
+++ src/main/java/com/example/gitflow/demo/DemoApplication.java
@@ -3,6 +3,13 @@ package com.example.gitflow.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * Clase principal del proyecto
+ * Contine método  main
+ * 
+ * */
+
+
 @SpringBootApplication
 public class DemoApplication {
 
