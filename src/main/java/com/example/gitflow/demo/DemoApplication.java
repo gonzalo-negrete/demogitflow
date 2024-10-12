03b893f94b0e4d374b04366640b2a3d051d24a35
Gonzalo <gonzalonegrete90@gmail.com>
2024-10-11 18:20:48 -0600

    Aplicación demo

--- src/main/java/com/example/gitflow/demo/DemoApplication.java
+++ src/main/java/com/example/gitflow/demo/DemoApplication.java
@@ -4,9 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /*
- * Clase principal que alberga el método main
+ * Clase principal que alberga el mï¿½todo main
  *
  * */
+
+ /**
+  * Hola, este es un ejemplo
+  */
 
 @SpringBootApplication
 public class DemoApplication {
